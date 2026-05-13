4fc5304b0af7ffef2a283a6f2882646ddbd6c279
Andrew Fan <andrewkongfan@gmail.com>
2026-05-12 20:33:25 -0700

    train on tinystories

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 from torch.utils.data import DataLoader
 import torch
 import torch.nn.functional as F
@@ -10,16 +11,24 @@ from datasets import load_dataset
 from data import Tokenize
 
 parser = argparse.ArgumentParser(description='Train Transformer')
-parser.add_argument('--num_heads', type=int, default=8, help='Number of attention heads')
-parser.add_argument('--embed_dim', type=int, default=256, help='Embedding dimension')
-parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
-parser.add_argument('--num_transformers', type=int, default=8, help='Number of transformer layers')
+parser.add_argument('--num_heads', type=int, default=4, help='Number of attention heads')
+parser.add_argument('--embed_dim', type=int, default=128, help='Embedding dimension')
+parser.add_argument('--batch_size', type=int, default=64, help='Batch size')
+parser.add_argument('--mini_batch_size', type=int, default=16, help='Mini batch size')
+parser.add_argument('--num_transformers', type=int, default=4, help='Number of transformer layers')
 args = parser.parse_args()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+if(torch.cuda.is_available()):
+    device = torch.device("cuda")
+elif(torch.backends.mps.is_available() and torch.backends.mps.is_built()):
+    device = torch.device("mps")
 
-context_size = 32
-train_dataset = load_dataset("Salesforce/wikitext", "wikitext-2-raw-v1", split="train")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+context_size = 128
+# train_dataset = load_dataset("Salesforce/wikitext", "wikitext-2-raw-v1", split="train")
+train_dataset = load_dataset("roneneldan/TinyStories", name="default", split="train")
 train_dataset = train_dataset.with_format("torch")
 train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size)
 transform = Tokenize()
@@ -37,7 +46,9 @@ pad_token_idx = tokenizer.vocab_size-1
 
 model = Model(tokenizer.vocab_size, args.embed_dim, args.num_transformers, args.num_heads)
 
-epochs = 1
+steps_per_batch = args.batch_size // args.mini_batch_size
+
+epochs = 5
 optim = Adam(model.parameters(), lr=0.001)
 loss_fn = CrossEntropyLoss(ignore_index=pad_token_idx)
 
@@ -46,6 +57,7 @@ model = model.to(device)
 
 avg_loss = 0
 steps = 0
+batch_steps = 0
 
 for i in range(epochs):
     for batch in train_dataloader:
@@ -57,21 +69,25 @@ for i in range(epochs):
         x = x.to(device)
         y = y.to(device)
 
-        optim.zero_grad()
 
         pred = model(x)
         pred = pred.flatten(0, 1)
         y = y.flatten(0, 1)
-        # print(pred.shape, y.shape)
         loss = loss_fn(pred, y)
         avg_loss += loss
 
         loss.backward()
-        optim.step()
+        
+        steps += 1
+        batch_steps += 1
+        if(batch_steps % steps_per_batch):
+            optim.step()
+            optim.zero_grad()
 
-        if(steps % 20 == 19):
+        if(steps % 20 == 0):
             avg_loss /= 20
-            torch.save(model.state_dict(), "./saved/test_model.pt")
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            torch.save(model.state_dict(), f"./saved/test_model_{timestamp}.pt")
             print(f"Epoch {i} training loss: {avg_loss}")
             avg_loss = 0
   
